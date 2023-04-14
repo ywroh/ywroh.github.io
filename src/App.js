5405579bedbd11963532087f60947276b983ba8d
YongWoo Roh <ywroh2@gmail.com>
2023-04-14 22:03:09 +0900

    Update mainPage

--- src/App.js
+++ src/App.js
@@ -46,8 +46,12 @@ function App() {
       <div className="introduce">
         <p>Hello. I am YongWoo Roh.</p>
         <p>I am working as a startup developer in Seoul, Korea.</p>
-        <p>I don't know anything. - Daniel Stenberg</p>
-        <p>Ignoramus et ignorabimus. - Emil du Bois-Reymond</p>
+        <p>"I don't know anything." - Daniel Stenberg</p>
+        <p>"Ignoramus et ignorabimus." - Emil du Bois-Reymond</p>
+        <p>
+          "Success is not how high you have climbed, but how you make a positive
+          difference to the world." - Roy T. Bennett
+        </p>
       </div>
       {/** Add font awesome icons  */}
       <div className="related">
